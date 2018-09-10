08031d3e44a09c6592d0b019b6683b4b4e5a7b21
Rafael S. de Souza <rafael.2706@gmail.com>
2018-09-10 15:17:33 -0400

    tda

--- Topological.R
+++ Topological.R
@@ -2,41 +2,77 @@ require(TDA)
 
 wise_train <- read.table('WISE_train.txt', head=TRUE, fill=TRUE)[,4:7]
 wise_test1 <- read.table('WISE_test1.txt', head=TRUE, fill=TRUE)[,4:7]
+wise_test2 <- read.table('WISE_test2.txt', head=TRUE, fill=TRUE)[,4:7]
+wise_test3 <- read.table('WISE_test3.txt', head=TRUE, fill=TRUE)[,4:7]
 wise_test4 <- read.table('WISE_test4.txt', head=TRUE, fill=TRUE)[,4:7]
 
 
-index <- sample(1:1000, 100,replace=F)
+index <- sample(1:1000, 150,replace=F)
 
 
-Diag1 <- ripsDiag(X = wise_train[index,], maxdimension = 2, maxscale = 5)
-plot(Diag1[["diagram"]])
-
-Diag2 <- ripsDiag(X = wise_test1[index,], maxdimension = 2, maxscale = 5)
-plot(Diag2[["diagram"]])
-
+Diagtrain <- ripsDiag(X = wise_train[index,], maxdimension = 2, maxscale = 5)
+Diag1 <- ripsDiag(X = wise_test1[index,], maxdimension = 2, maxscale = 5)
+Diag2 <- ripsDiag(X = wise_test2[index,], maxdimension = 2, maxscale = 5)
+Diag3 <- ripsDiag(X = wise_test3[index,], maxdimension = 2, maxscale = 5)
 Diag4 <- ripsDiag(X = wise_test4[index,], maxdimension = 2, maxscale = 5)
-plot(Diag4[["diagram"]])
 
-print(bottleneck(Diag1 = Diag1[["diagram"]], Diag2 = Diag4[["diagram"]],
+
+
+TD1 <- print(bottleneck(Diag1 = Diagtrain[["diagram"]], Diag2 = Diag1[["diagram"]],
                  dimension = 1))
+TD2 <- print(bottleneck(Diag1 = Diagtrain[["diagram"]], Diag2 = Diag2[["diagram"]],
+                        dimension = 1))
+TD3 <- print(bottleneck(Diag1 = Diagtrain[["diagram"]], Diag2 = Diag3[["diagram"]],
+                        dimension = 1))
+TD4 <- print(bottleneck(Diag1 = Diagtrain[["diagram"]], Diag2 = Diag4[["diagram"]],
+                        dimension = 1))
 
 
-print(wasserstein(Diag1 = Diag1[["diagram"]], Diag2 = Diag4[["diagram"]],
-                  p = 2, dimension = 1))
+
+
+pdf("TDA.pdf",height=20,width = 15)
+par(mfrow=c(5,4))
+
+attach(wise_train)
+plot(Diagtrain[["diagram"]],main="Model")
+plot(w1mpro-w2mpro, w2mpro-w3mpro, pch=20, cex=0.5, main='Training set', col='#11111120') # infrared color-color diagram
+plot(w2mpro-w3mpro, w3mpro-w4mpro, pch=20, cex=0.5, col='#11111120')  # infrared  color-color diagram
+plot(w2mpro, w2mpro-w3mpro, pch=20, cex=0.2, col='#11111120')  # infrared color-magnitude diagram (with truncation)
+
+attach(wise_test1)
+plot(Diag1[["diagram"]],main=paste("Bottleneck distance to model = ",round(TD1,3)))
+plot(w1mpro-w2mpro, w2mpro-w3mpro, pch=20, cex=0.5, main='Test set 1', col='#11111120') # infrared color-color diagram
+plot(w2mpro-w3mpro, w3mpro-w4mpro, pch=20, cex=0.5, col='#11111120')  # infrared  color-color diagram
+plot(w2mpro, w2mpro-w3mpro, pch=20, cex=0.5, col='#11111120')  # infrared color-magnitude diagram (with truncation)
+
+attach(wise_test2)
+plot(Diag2[["diagram"]],main=paste("Bottleneck distance to model = ",round(TD2,3)))
+plot(w1mpro-w2mpro, w2mpro-w3mpro, pch=20, cex=0.5, main='Test set 2', col='#11111120') # infrared color-color diagram
+plot(w2mpro-w3mpro, w3mpro-w4mpro, pch=20, cex=0.5, col='#11111120')  # infrared  color-color diagram
+plot(w2mpro, w2mpro-w3mpro, pch=20, cex=0.5, col='#11111120')  # infrared color-magnitude diagram (with truncation)
+
+attach(wise_test3)
+plot(Diag3[["diagram"]],main=paste("Bottleneck distance to model = ",round(TD3,3)))
+plot(w1mpro-w2mpro, w2mpro-w3mpro, pch=20, cex=0.5, main='Test set 3', col='#11111120') # infrared color-color diagram
+plot(w2mpro-w3mpro, w3mpro-w4mpro, pch=20, cex=0.52, col='#11111120')  # infrared  color-color diagram
+plot(w2mpro, w2mpro-w3mpro, pch=20, cex=0.5, col='#11111120')  # infrared color-magnitude diagram (with truncation)
+
+
+attach(wise_test4)
+plot(Diag4[["diagram"]],main=paste("Bottleneck distance to model = ",round(TD4,3)))
+
+plot(w1mpro-w2mpro, w2mpro-w3mpro, pch=20, cex=0.5, main='Test set 4', col='#11111120') # infrared color-color diagram
+plot(w2mpro-w3mpro, w3mpro-w4mpro, pch=20, cex=0.5, col='#11111120')  # infrared  color-color diagram
+plot(w2mpro, w2mpro-w3mpro, pch=20, cex=0.5, col='#11111120')  # infrared color-magnitude diagram (with truncation)
+
+dev.off()
 
 
 
-Diag <- matrix(c(0, 0, 10, 1, 0, 3, 1, 3, 8), ncol = 3, byrow = TRUE)
-DiagLim <- 10
-colnames(Diag) <- c("dimension", "Birth", "Death")
 
-#persistence landscape
-tseq <- seq(0,DiagLim, length = 1000)
-Land <- landscape(Diag, dimension = 1, KK = 1, tseq)
 
-par(mfrow = c(1,2))
-plot.diagram(Diag)
-plot(tseq, Land, type = "l", xlab = "t", ylab = "landscape", asp = 1)
+
+
 
 
  
